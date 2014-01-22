b8a1d93ec0792d19ce4b5898fb11636fa6e9ee96
Josh Toft <josh@pack.io>
2014-01-22 01:35:16 -0800

    Saving state

--- config.go
+++ config.go
@@ -1,26 +1,41 @@
 package gapp
 
 import (
-	"errors"
-	"fmt"
-	"reflect"
+	// "errors"
+	// "fmt"
 	"sync"
 )
 
-type ConfigEntry struct {
+type _ConfigEntry struct {
+	Short string
+	Description string
 	Value interface{}
-	Type  reflect.Kind
+	Default interface{}
+	Required bool
+	Listeners []chan interface{}
 }
 
 type _Config struct {
 	sync.RWMutex
-	entries map[string]ConfigEntry
+	entries map[string]_ConfigEntry
 }
 
 var Config *_Config
 
+func DefaultEntry() _ConfigEntry {
+	return _ConfigEntry{
+		Long: "",
+		Short: "",
+		Description: "",
+		Value: nil,
+		Default: nil,
+		Required: false,
+		Listeners: make([]chan interface{}, 0),
+	}
+}
+
 func _Load() {
-	Config = &_Config{entries: make(map[string]ConfigEntry)}
+	Config = &_Config{entries: make(map[string]_ConfigEntry)}
 }
 
 func init() {
@@ -36,22 +51,41 @@ func (c *_Config) keys() []string {
 	return klist
 }
 
-// Sets a configuration parameter
-func (c *_Config) set(k string, v interface{}, t reflect.Kind) (ConfigEntry, error) {
-	entry := ConfigEntry{Value: v, Type: t}
-	if t == reflect.ValueOf(v).Kind() {
-		c.Lock()
-		defer c.Unlock()
-		c.entries[k] = entry
-		return entry, nil
-	}
-	error_string := fmt.Sprintf("gapp: %s is not a kind of %s", v, t)
-	return ConfigEntry{}, errors.New(error_string)
+// Add up a new configuration parameter
+func (c *_Config) add(option, short, description string, dfault interface{}, required bool) (_ConfigEntry, error) {
+	entry := DefaultEntry()
+	entry.Long = option
+	entry.Short = short
+	entry.Description = description
+	entry.Value = dfault
+	entry.Required = required
+	c.Lock()
+	defer c.Unlock()
+	c.entries[option] = entry
+	return entry, nil
 }
 
-// Gets a configuration parameter
-func (c *_Config) get(k string) (ConfigEntry) {
-	c.RLock()
-	defer c.RUnlock()
-	return c.entries[k]
-}
+// // Sets a configuration parameter
+// func (c *_Config) set(k string, v interface{}) (_ConfigEntry, error) {
+// 	entry := ConfigEntry{Value: v, Type: t}
+// 	if t == reflect.ValueOf(v).Kind() {
+// 		c.Lock()
+// 		defer c.Unlock()
+// 		c.entries[k] = entry
+// 		return entry, nil
+// 	}
+// 	error_string := fmt.Sprintf("gapp: %s is not a kind of %s", v, t)
+// 	return ConfigEntry{}, errors.New(error_string)
+// }
+
+// // Gets information for a configuration parameter
+// func (c *_Config) info(k string) (ConfigEntry) {
+// 	c.RLock()
+// 	defer c.RUnlock()
+// 	return c.entries[k]
+// }
+
+// // Gets a configuration value
+// func (c *_Config) get(k string) (interface{}) {
+// 	return c.info(k).Value
+// }
